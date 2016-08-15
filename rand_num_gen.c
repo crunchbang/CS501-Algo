4e9db3d13f13b5f664d2686cceed52c2f5736e18
Athul Suresh <athulsure@gmail.com>
2016-08-15 05:42:30 +0530

    Initial commit

---
+++ rand_num_gen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+        long n = atoi(argv[1]);
+        FILE *fp = fopen(argv[2], "w");
+        long i;
+        long limit = pow(10, n);
+        srand(time(NULL));
+        for (i = 0; i < limit; ++i) {
+                int r = rand() % 10;
+                fprintf(fp, "%d", r);
+        }
+
+
+}
